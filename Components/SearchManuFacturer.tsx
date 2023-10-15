82816b580004ae2770acb838be4106544b67d142
hrfahimm <fahimtaj10@gmail.com>
2023-10-15 20:35:58 +0600

    ewwww

--- Components/SearchManuFacturer.tsx
+++ Components/SearchManuFacturer.tsx
@@ -23,7 +23,9 @@ const SearchManuFacturer = ({
               );
     return (
         <div className='search-manufacturer'>
-            <Combobox>
+            <Combobox
+                value={manufacturer}
+                onChange={setManufacturer}>
                 <div className='relative w-full'>
                     <Combobox.Button className='absolute top-[14px]'>
                         <Image
@@ -67,7 +69,26 @@ const SearchManuFacturer = ({
                                 }
                                 `}
                                         value={item}>
-                                        {item}
+                                        {({ selected, active }) => (
+                                            <>
+                                                <span
+                                                    className={`block truncate ${
+                                                        selected
+                                                            ? "font-medium"
+                                                            : "font-normal"
+                                                    }`}>
+                                                    {item}
+                                                </span>
+                                                {selected ? (
+                                                    <span
+                                                        className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                                            active
+                                                                ? "text-white"
+                                                                : "text-teal-600"
+                                                        }`}></span>
+                                                ) : null}
+                                            </>
+                                        )}
                                     </Combobox.Option>
                                 ))
                             )}
